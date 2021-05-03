ee500ae41d9cf1f95bf82da9b6b9fa65fa2cf50b
YuriiDotsenko <yuriidotsenko59@gmail.com>
2021-05-03 19:35:42 +0200

    Зробив розмітку меню і зміну кольору теми

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
 import "../src/styles.css";
 import menu from "./menu.json";
 import renderMenu from "./templates/card.hbs";
+import Theme from "./localstorage";
 
 const toogleRef = document.querySelector(".theme-switch__toggle");
 const body = document.querySelector("body");
+const containerMenuCards = document.querySelector(".js-menu");
 
-// const Theme = {
-//   LIGHT: "light-theme",
-//   DARK: "dark-theme",
-// };
+const cardMarkup = createCardMarcup(menu);
+
+containerMenuCards.insertAdjacentHTML("beforeend", cardMarkup);
 
 toogleRef.addEventListener("change", onClickChangeColor);
 
 function onClickChangeColor(evt) {
   if (evt.target.checked) {
-    body.classList.add(".dark-theme");
-    body.classList.remove(".light-theme");
+    body.classList.add("dark-theme");
+    body.classList.remove("light-theme");
+
+    localStorage.setItem("theme", "dark-theme");
   } else {
-    body.classList.remove(".dark-theme");
-    body.classList.add(".light-theme");
+    body.classList.remove("dark-theme");
+    body.classList.add("light-theme");
+
+    localStorage.setItem("theme", "light-theme");
   }
+}
+
+function createCardMarcup(menu) {
+  return menu.map(renderMenu).join("");
 }
---
+++ src/localstorage.js
@@ -0,0 +1,18 @@
+// const body = document.querySelector("body");
+
+// const Theme = {
+//   LIGHT: "light-theme",
+//   DARK: "dark-theme",
+// };
+
+// getColorThemeLocalStorage();
+
+// const STORAGE_KEY = "Theme";
+
+// function getColorThemeLocalStorage() {
+//   const themeStatus = localStorage.getItem(STORAGE_KEY);
+
+//   if (themeStatus !== null && themeStatus === Theme.DARK) {
+//     document.body.className = themeStatus;
+//   }
+// }
