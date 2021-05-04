5b5df35d829ee8a03f7f40f89f8431ea10495fcd
YuriiDotsenko <yuriidotsenko59@gmail.com>
2021-05-04 19:09:50 +0200

    Доробив дз

--- src/index.js
+++ src/index.js
@@ -1,31 +1,13 @@
 import "../src/styles.css";
 import menu from "./menu.json";
 import renderMenu from "./templates/card.hbs";
-import Theme from "./localstorage";
+import switchTheme from "./theme.js";
 
-const toogleRef = document.querySelector(".theme-switch__toggle");
-const body = document.querySelector("body");
 const containerMenuCards = document.querySelector(".js-menu");
 
 const cardMarkup = createCardMarcup(menu);
 
 containerMenuCards.insertAdjacentHTML("beforeend", cardMarkup);
-
-toogleRef.addEventListener("change", onClickChangeColor);
-
-function onClickChangeColor(evt) {
-  if (evt.target.checked) {
-    body.classList.add("dark-theme");
-    body.classList.remove("light-theme");
-
-    localStorage.setItem("theme", "dark-theme");
-  } else {
-    body.classList.remove("dark-theme");
-    body.classList.add("light-theme");
-
-    localStorage.setItem("theme", "light-theme");
-  }
-}
 
 function createCardMarcup(menu) {
   return menu.map(renderMenu).join("");
