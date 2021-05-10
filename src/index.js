2f98aa0037086a17a399f1c40fe991e2c06dbd9c
YuriiDotsenko <yuriidotsenko59@gmail.com>
2021-05-10 15:25:00 +0200

    добавив настройки gh-pages

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "../src/styles.css";
 import menu from "./menu.json";
 import renderMenu from "./templates/card.hbs";
 import switchTheme from "./theme.js";
+const ghpages = require("gh-pages");
 
 const containerMenuCards = document.querySelector(".js-menu");
 
