0b3acd344198e9148de32a5f0f904af55677533c
YuriiDotsenko <yuriidotsenko59@gmail.com>
2021-05-27 19:01:31 +0200

    зробив збереження тем в localStorage

--- src/theme.js
+++ src/theme.js
@@ -1,29 +1,41 @@
-const body = document.querySelector("body");
-const checkbox = document.querySelector(".theme-switch__toggle");
+const body = document.querySelector('body');
+const checkbox = document.querySelector('.theme-switch__toggle');
 
 const Theme = {
-  LIGHT: "light-theme",
-  DARK: "dark-theme",
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
 };
 
-let currentTheme = localStorage.getItem("theme");
-if (JSON.parse(currentTheme) === Theme.DARK) {
-  checkbox.checked = true;
-  body.setAttribute("class", Theme.DARK);
-} else {
-  body.setAttribute("class", Theme.LIGHT);
+checkbox.addEventListener('change', changeTheme);
+
+function changeTheme(event) {
+  event.currentTarget.checked ? checkedInput() : notCheckedInput();
 }
 
-const changeTheme = function (evt) {
-  if (evt.target.checked) {
-    body.classList.add(Theme.DARK);
-    body.classList.remove(Theme.LIGHT);
-    localStorage.setItem("theme", JSON.stringify(Theme.DARK));
-  } else {
-    body.classList.add(Theme.LIGHT);
-    body.classList.remove(Theme.DARK);
-    localStorage.setItem("theme", JSON.stringify(Theme.LIGHT));
-  }
-};
+function checkedInput() {
+  body.classList.add(Theme.DARK);
+  body.classList.remove(Theme.LIGHT);
+  localStorage.setItem('theme', Theme.DARK);
+  checkbox.checked = true;
+}
 
-checkbox.addEventListener("change", changeTheme);
+function notCheckedInput() {
+  body.classList.add(Theme.LIGHT);
+  body.classList.remove(Theme.DARK);
+  localStorage.setItem('theme', Theme.LIGHT);
+  checkbox.checked = false;
+}
+
+function carrentTheme() {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === Theme.LIGHT || savedTheme === null) {
+    notCheckedInput();
+    return;
+  }
+  if (savedTheme === Theme.DARK) {
+    checkedInput();
+    return;
+  }
+}
+
+carrentTheme();
